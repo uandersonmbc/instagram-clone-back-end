460a873cfe3797c0f77b7b57d852aeeb03db6813
Uanderson Nunes <uandersonmbc@hotmail.com>
2019-06-12 22:27:58 -0300

    Post, get e put funcionado já

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,11 +1,34 @@
 const Post = require('../models/Post');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
-    async index(req, res){
-
+    async index(req, res) {
+        const post = await Post.find().sort('-createdAt');
+        return res.json(post);
     },
 
-    async store(req, res){
+    async store(req, res) {
+        const { author, place, description, hashtags } = req.body;
+        const { filename: image } = req.file;
 
+        await sharp(req.file.path)
+            .resize(500)
+            .jpeg({ quality: 85 })
+            .toFile(
+                path.resolve(req.file.destination, 'resized', image)
+            );
+
+        fs.unlinkSync(req.file.path);
+
+        const post = await Post.create({
+            author,
+            place,
+            description,
+            hashtags,
+            image,
+        });
+        return res.json(post);
     }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+const PostController = require('./controllers/PostController');
+const LikeController = require('./controllers/LikeController');
 
 const routes = new express.Router();
 
-routes.get('/', (req, res) =>{
-    return res.send('Olá mundo');
-})
+const upload = multer(uploadConfig);
+
+routes.post('/post', upload.single('image'), PostController.store);
+routes.get('/post', PostController.index);
+routes.put('/post/:id/like', LikeController.like);
 
 module.exports = routes;
